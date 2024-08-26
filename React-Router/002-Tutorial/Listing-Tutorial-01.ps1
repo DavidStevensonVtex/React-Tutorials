1bcdc049b09160f713e486ec4254602075976eea
David Stevenson <drsgis@rit.edu>
2024-08-26 10:11:47 -0400

    Tutorial - Setup new React + Vite project using npm script

---
+++ React-Router/002-Tutorial/Listing-Tutorial-01.ps1
@@ -0,0 +1,6 @@
+npm create vite@latest myapp  -- --template react
+# follow prompts
+Set-Location myapp
+npm install react-router-dom # always need this!
+npm install localforage match-sorter sort-by # only for this tutorial.
+npm run dev
